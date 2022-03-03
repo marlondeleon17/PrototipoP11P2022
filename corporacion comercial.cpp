2ebe2514ca098e46d8eaef3fd656812d07fd67a2
marlondeleon17 <mdeleong17@mi.umg.gt>
2022-03-03 13:36:22 -0600

    nueva actualizacion

--- corporacion comercial.cpp
+++ corporacion comercial.cpp
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#define N 6
 main()
 {
-int=6
+
+
 int v1 [N] = {0, 2, 4, 1, 3, 5};
 int v2 [N] = {11, 22, 33, 44, 55, 66};
 int x = 1;
+printf ("corporacion comercial "  );
+printf ("            ");
+printf ("Marlon Rubén De león González 9959-19-460");
 for(x=0; x <N; x++)
+
 printf("%d , ", v2[v1[x]]);
 
 return 0;
